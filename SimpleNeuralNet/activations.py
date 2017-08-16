f257310befa9aed9e870be56fa86e55e474c2d1e
Terrence Ho <terrenceho.books@gmail.com>
2017-08-15 23:14:45 -0700

    -Changed activation functions to computer both derivative and activations

--- SimpleNeuralNet/activations.py
+++ SimpleNeuralNet/activations.py
@@ -23,70 +23,55 @@ last layer since it is numerically unsable
 #     return np.tanh(w)
 
 
-def sigmoid(Z):
+def sigmoid(dA, Z, derivative=False):
     """
     Implements the sigmoid activation in numpy
     
     Arguments:
-    Z -- numpy array of any shape
-    
-    Returns:
-    A -- output of sigmoid(z), same shape as Z
-    cache -- returns Z as well, useful during backpropagation
-    """
-    A = 1/(1+np.exp(-Z))
-    cache = Z
-    return A, cache
-
-def relu(Z):
-    """
-    Implement the RELU function.
-
-    Arguments:
-    Z -- Output of the linear layer, of any shape
+    dA -- post-activation gradient, of any shape
+    Z -- 'Z' where we store for computing backward propagation efficiently. Also
+        called cache
+    derivative -- bool to determine if we are computing derivative or not 
 
     Returns:
-    A -- Post-activation parameter, of the same shape as Z
-    cache -- a python dictionary containing "A" ; stored for computing the backward pass efficiently
+    if derivative == False:
+        A -- output of sigmoid(z), same shape as Z
+        cache -- returns Z as well, useful during backpropagation
+    if derivative == True:
+        dZ -- Gradient of the cost with respect to Z
     """
-    
-    A = np.maximum(0,Z)
-    cache = Z 
-    return A, cache
 
+    if derivative:
+        s = 1/(1+np.exp(-Z))
+        dZ = dA * s * (1-s)
+        return dZ
+    else:
+        A = 1/(1+np.exp(-Z))
+        cache = Z
+        return A, cache
 
-def relu_backward(dA, cache):
+def relu(dA, Z, derivative=False):
     """
-    Implement the backward propagation for a single RELU unit.
+    Implement the RELU function or implement the backward propagation for a single RELU unit.
 
     Arguments:
     dA -- post-activation gradient, of any shape
-    cache -- 'Z' where we store for computing backward propagation efficiently
+    Z -- Output of the linear layer, of any shape.  Also called cache
+    derivative -- bool to determine if we are computing derivative or not 
 
     Returns:
-    dZ -- Gradient of the cost with respect to Z
+    if derivative == False:
+        A -- Post-activation parameter, of the same shape as Z
+        cache -- a python dictionary containing "A" ; stored for computing the backward pass efficiently
+    if derivative == True:
+        dZ -- Gradient of the cost with respect to Z
     """
-    Z = cache
-    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
-    # When z <= 0, you should set dz to 0 as well. 
-    dZ[Z <= 0] = 0
-    return dZ
-
-def sigmoid_backward(dA, cache):
-    """
-    Implement the backward propagation for a single SIGMOID unit.
-
-    Arguments:
-    dA -- post-activation gradient, of any shape
-    cache -- 'Z' where we store for computing backward propagation efficiently
-
-    Returns:
-    dZ -- Gradient of the cost with respect to Z
-    """
-    
-    Z = cache
-    s = 1/(1+np.exp(-Z))
-    dZ = dA * s * (1-s)
-    assert (dZ.shape == Z.shape)
-    return dZ
-
+    if derivative:
+        dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+        # When z <= 0, you should set dz to 0 as well. 
+        dZ[Z <= 0] = 0
+        return dZ
+    else:
+        A = np.maximum(0,Z)
+        cache = Z 
+        return A, cache
